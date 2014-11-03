76d1392051d7c05876219cfd14e26592cc264bfc
Jason Yeomans <jyeo098@gmail.com>
2014-11-03 03:45:34 -0500

    Initial Commit, add all files

---
+++ js/jq.js
@@ -0,0 +1,18 @@
+function equalHeight(group) {
+    var tallest = 0;
+    group.each(function() {
+        var thisHeight = $(this).height();
+        if(thisHeight > tallest) {
+            tallest = thisHeight;
+        }
+    });
+    group.each(function() { $(this).height(tallest); });
+}
+
+
+
+$(document).ready(function() {
+  $('#top-jumbo').css('height', window.innerHeight + 'px');
+  $('#top-bg').css('height', window.innerHeight + 'px');
+  equalHeight($(".thumbnail")); 
+});
