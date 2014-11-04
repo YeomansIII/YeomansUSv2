77fe2796d5309f8adf49bdb5cdde5051eb8c87a3
Jason Yeomans <jyeo098@gmail.com>
2014-11-04 13:19:19 -0500

    Trying to navbar color change on scroll

--- js/jq.js
+++ js/jq.js
@@ -1,12 +1,14 @@
 function equalHeight(group) {
-    var tallest = 0;
-    group.each(function() {
-        var thisHeight = $(this).height();
-        if(thisHeight > tallest) {
-            tallest = thisHeight;
-        }
-    });
-    group.each(function() { $(this).height(tallest); });
+  var tallest = 0;
+  group.each(function() {
+    var thisHeight = $(this).height();
+    if (thisHeight > tallest) {
+      tallest = thisHeight;
+    }
+  });
+  group.each(function() {
+    $(this).height(tallest);
+  });
 }
 
 
@@ -14,5 +16,20 @@ function equalHeight(group) {
 $(document).ready(function() {
   $('#top-jumbo').css('height', window.innerHeight + 'px');
   $('#top-bg').css('height', window.innerHeight + 'px');
-  equalHeight($(".thumbnail")); 
+  equalHeight($(".thumbnail"));
+
+    var mainbottom = 300;
+
+  $(window).on('scroll', function() {
+
+    // we round here to reduce a little workload
+    stop = Math.round($(window).scrollTop());
+    if (stop > mainbottom) {
+      $('.navbar-default').addClass('past-main');
+    } else {
+      $('.navbar-default').removeClass('past-main');
+    }
+
+  });
+
 });
